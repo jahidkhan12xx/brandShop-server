4c32bcb4b1960d384fce9b687ba908468b83fe17
Jahid Kham <jahidkhan12xx@gmail.com>
2023-10-18 12:51:35 +0600

    prductData added

---
+++ index.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 3000;
+
+// MiddleWare
+app.use(cors());
+app.use(express.json());
+
+
+// MongoDB
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.f3op28d.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    
+    await client.connect();
+
+    const database = client.db("AddProducts").collection("Products");
+
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = {
+        
+        brand_name:  (id),
+      };
+      const result = await database.findOne(query);
+      console.log(result);
+      res.send(result);
+    });
+
+    app.get("/products", async (req, res) => {
+      const result = await database.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/products", async (req, res) => {
+      const product = req.body;
+      const result = await database.insertOne(product);
+      console.log(result);
+      res.send(result);
+    });
+
+    
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
